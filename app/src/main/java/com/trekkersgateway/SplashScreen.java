44d781461c9d92b455a89790a039ce19bb094be4
RazzibShakya <rajipshakya@gmail.com>
2019-04-23 22:01:27 +0545

    Design and Splash screen

---
+++ app/src/main/java/com/trekkersgateway/SplashScreen.java
@@ -0,0 +1,23 @@
+package com.trekkersgateway;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        final Intent intent = new Intent(SplashScreen.this, Login.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
