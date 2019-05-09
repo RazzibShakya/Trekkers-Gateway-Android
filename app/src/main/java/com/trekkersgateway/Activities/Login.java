eea0ed899bb5261a46f82357bc16d8631dd48c8c
RazzibShakya <rajipshakya@gmail.com>
2019-05-09 21:32:39 +0545

    Design and Splash screen

---
+++ app/src/main/java/com/trekkersgateway/Activities/Login.java
@@ -0,0 +1,73 @@
+package com.trekkersgateway.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.trekkersgateway.R;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    EditText txtusername, pwpass;
+    Button btnlogin;
+    TextView btnregister, btnforget;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+        initiate();
+    }
+
+    public void initiate() {
+        txtusername = findViewById(R.id.txtuser);
+        pwpass = findViewById(R.id.txtpass);
+        btnlogin = findViewById(R.id.btnLogin);
+        btnregister = findViewById(R.id.btnregister);
+
+        btnlogin.setOnClickListener(this);
+        btnregister.setOnClickListener(this);
+    }
+
+    public void Login(){
+      if(txtusername.getText().toString().isEmpty()){
+          txtusername.setError("Enter Username");
+          txtusername.requestFocus();
+      }
+      else if(pwpass.getText().toString().isEmpty()){
+          pwpass.setError("Enter Password");
+          pwpass.requestFocus();
+      }
+      else if(pwpass.getText().toString().isEmpty()&&pwpass.getText().toString().isEmpty()){
+            txtusername.setError("Enter Password");
+            pwpass.setError("Enter Password");
+            txtusername.requestFocus();
+        }else{
+
+
+      }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnLogin:
+                Login();
+
+                break;
+            case R.id.btnregister:
+
+
+                break;
+            case R.id.btnforgot:
+
+
+
+                break;
+        }
+    }
+}
--- app/src/main/java/com/trekkersgateway/Activities/SplashScreen.java
+++ app/src/main/java/com/trekkersgateway/Activities/SplashScreen.java
@@ -1,9 +1,11 @@
-package com.trekkersgateway;
+package com.trekkersgateway.Activities;
 
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.trekkersgateway.R;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -11,6 +13,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
         final Intent intent = new Intent(SplashScreen.this, Login.class);
         new Handler().postDelayed(new Runnable() {
             @Override
