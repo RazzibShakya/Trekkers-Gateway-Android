04c89f929e596ea8787855e8d193943c6151aac7
RazzibShakya <rajipshakya@gmail.com>
2019-06-05 23:54:03 +0545

    Login Code

--- app/src/main/java/com/trekkersgateway/Activities/Dashboard.java
+++ app/src/main/java/com/trekkersgateway/Activities/Dashboard.java
@@ -1,15 +1,52 @@
 package com.trekkersgateway.Activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.trekkersgateway.R;
 
 public class Dashboard extends AppCompatActivity {
 
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_treks:
+                    mTextMessage.setText("All Treks");
+                    return true;
+                case R.id.navigation_forum:
+                    mTextMessage.setText("Forums");
+                    return true;
+                case R.id.navigation_feed:
+                    mTextMessage.setText("Photos feed");
+                    return true;
+                case R.id.navigation_news:
+                    mTextMessage.setText("News Feed");
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText("Profile");
+                    return true;
+            }
+            return false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
