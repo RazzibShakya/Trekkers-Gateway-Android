a1917bf5a4875dbb1f6e8f0a0b607100b33f7bae
RazzibShakya <rajipshakya@gmail.com>
2019-06-23 01:13:19 +0545

    Feed Model

--- app/src/main/java/com/trekkersgateway/Model/UserAPI.java
+++ app/src/main/java/com/trekkersgateway/Model/UserAPI.java
@@ -1,12 +1,15 @@
 package com.trekkersgateway.Model;
 
 
+import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 
 public interface UserAPI {
@@ -14,9 +17,18 @@ public interface UserAPI {
     @POST("createuser")
     Call<Void>addNewUser(@Body User user);
 
+    @POST("feed")
+    Call<Void> addFeed(@Body Feed feed);
+
    //for login
     @FormUrlEncoded
     @POST("login")
     Call<LoginResponse>checkUser(@Field("username")String username,@Field("password")String password);
+
+
+    //this is a post method which will upload pic to server or API
+    @Multipart
+    @POST("uploadpic")
+    Call<ImageFile> uploadImage(@Part MultipartBody.Part body);
 
 }
